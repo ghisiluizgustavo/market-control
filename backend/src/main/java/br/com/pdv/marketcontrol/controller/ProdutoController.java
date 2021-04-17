e8c433a8f6a85f40eeef7ccd5d6a111c0a131d23
Luiz Gustavo Ghisi Flores <ghisiluizgustavo@gmail.com>
2021-04-16 22:31:15 -0300

    👀 (fix): alterações para funcionameto no mysql

--- backend/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
+++ backend/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
@@ -1,2 +1,15 @@
-package br.com.pdv.marketcontrol.controller;public class ProdutoController {
+package br.com.pdv.marketcontrol.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @GetMapping()
+    public String listAllProdutos(){
+        return "listar todos";
+    }
 }
