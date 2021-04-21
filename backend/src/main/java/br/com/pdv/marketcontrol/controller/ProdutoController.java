a5c17144e3d6180533f8d31ff030542c9e94ab55
Luiz Gustavo <ghisiluizgustavo@gmail.com>
2021-04-21 12:47:29 -0300

    🚧 (feat): construindo controller

--- backend/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
+++ backend/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
@@ -1,15 +1,14 @@
 package br.com.pdv.marketcontrol.controller;
 
 import br.com.pdv.marketcontrol.model.Produto;
-import br.com.pdv.marketcontrol.model.Venda;
 import br.com.pdv.marketcontrol.repository.ProdutoRepository;
-import br.com.pdv.marketcontrol.repository.VendaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,10 +19,18 @@ public class ProdutoController {
     ProdutoRepository produtoRepository;
 
     @GetMapping()
-    public ResponseEntity<List<Produto>> listAllProdutos(){
+    public ResponseEntity<List<Produto>> listarProdutos(){
 
         List<Produto> listaProd = this.produtoRepository.findAll();
 
         return ResponseEntity.ok(listaProd);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Produto> salvarProduto(@RequestBody Produto produto, UriComponentsBuilder uriBuilder){
+        Produto produtoResultado = this.produtoRepository.save(produto);
+        URI uri = uriBuilder.path("/produtos/{id}").buildAndExpand(produtoResultado.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(produtoResultado);
     }
 }
