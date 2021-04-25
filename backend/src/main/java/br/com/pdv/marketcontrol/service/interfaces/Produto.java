b05a76c1aeb74ea2e90382da1b596bb5bb558fbc
Luiz Gustavo <ghisiluizgustavo@gmail.com>
2021-04-25 17:32:57 -0300

    🌱 (feat): iniciar services

--- backend/src/main/java/br/com/pdv/marketcontrol/service/interfaces/Produto.java
+++ backend/src/main/java/br/com/pdv/marketcontrol/service/interfaces/Produto.java
@@ -1,2 +1,10 @@
-package br.com.pdv.marketcontrol.service.interfaces;public interface Produto {
+package br.com.pdv.marketcontrol.service.interfaces;
+
+import br.com.pdv.marketcontrol.model.dto.ProdutoDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface Produto {
+
+    public Page<ProdutoDTO> findAll(Pageable pageable);
 }
