0146525790001a83897b4235d1e77fd69fdeb05a
Luiz Gustavo Ghisi <ghisiluizgustavo@gmail.com>
2021-05-01 23:35:37 -0300

    ✔️(feat): buscando produto por codigo de barras

--- backend/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
+++ backend/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package br.com.pdv.marketcontrol.controller;
 
 import br.com.pdv.marketcontrol.model.Produto;
+import br.com.pdv.marketcontrol.model.dto.ProdutoDTO;
 import br.com.pdv.marketcontrol.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,8 +9,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.swing.text.html.Option;
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -24,6 +27,17 @@ public class ProdutoController {
         List<Produto> listaProd = this.produtoRepository.findAll();
 
         return ResponseEntity.ok(listaProd);
+    }
+
+    @GetMapping("/{codBarras}")
+    public ResponseEntity<Produto> buscarProduto(@PathVariable String codBarras){
+        Optional<Produto> response = produtoRepository.findByCodBarras(codBarras);
+        //TODO -> Fazer uma função lambda(funcional)
+        if(response.isPresent()){
+            return ResponseEntity.ok().body(response.get());
+        } else {
+            return ResponseEntity.noContent().build();
+        }
     }
 
     @PostMapping()
--- backend/src/main/java/br/com/pdv/marketcontrol/repository/ProdutoRepository.java
+++ backend/src/main/java/br/com/pdv/marketcontrol/repository/ProdutoRepository.java
@@ -4,6 +4,10 @@ import br.com.pdv.marketcontrol.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    Optional<Produto> findByCodBarras(String codBarras);
 }
