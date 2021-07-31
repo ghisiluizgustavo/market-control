109bb870f186242df50eaa4171f91c4be6e05251
Luiz Gustavo Ghisi <ghisiluizgustavo@gmail.com>
2021-07-31 14:07:45 -0300

    (feat): pegar produto pelo codbarras

--- application/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
+++ application/src/main/java/br/com/pdv/marketcontrol/controller/ProdutoController.java
@@ -34,7 +34,6 @@ public class ProdutoController {
     }
 
     @GetMapping("/{codBarras}")
-    @ApiOperation("Busca um produto pelo código de barras")
     public ResponseEntity<ProdutoDTO> buscarProduto(@PathVariable String codBarras){
           return produtoService.buscarProduto(codBarras)
                   .map(produto -> ResponseEntity.ok().body(ProdutoDTO.converter(produto)))
@@ -42,7 +41,6 @@ public class ProdutoController {
     }
 
     @PostMapping()
-    @ApiOperation("Cadastra um novo produto")
     public ResponseEntity<ProdutoDTO> salvarProduto(
             @RequestBody Produto produto, UriComponentsBuilder uriBuilder){
         Produto prodResponse = this.produtoService.salvar(produto);
@@ -54,7 +52,6 @@ public class ProdutoController {
     }
 
     @PutMapping("/{id}")
-    @ApiOperation("Atualiza um produto pelo id")
     public ResponseEntity<ProdutoDTO> atualizarProduto(
             @PathVariable Long id, @RequestBody Produto produto){
         return produtoService.buscarId(id)
@@ -64,7 +61,6 @@ public class ProdutoController {
     }
 
     @DeleteMapping("/{id}")
-    @ApiOperation("Deleta um produto pelo id")
     public ResponseEntity deletarProduto(@PathVariable Long id){
          return produtoService.buscarId(id)
                  .map( produto -> produtoService.deletar(id))
